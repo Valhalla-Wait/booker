93a828d8a67f695a247dafc4dcfe2c2852b18e15
Valhalla-Wait <miha888g@yandex.ru>
2023-09-13 00:38:21 +0300

    refact3

--- src/pages/BookPage/index.tsx
+++ src/pages/BookPage/index.tsx
@@ -1,13 +1,17 @@
-import { Link, useParams } from "react-router-dom"
+import { useNavigate, useParams } from "react-router-dom"
 import { CategoryItem } from "../../components/BookItem/CategoryItem"
 import { useGetBookByIdQuery } from "../../store/slice/booksSlice"
 
 export const BookPage = () => {
     const { id } = useParams()
     const { data, isLoading } = useGetBookByIdQuery(id as string)
+    const navigate = useNavigate()
+    
+    const goBackHandler = () => navigate(-1)
+
     console.log(data)
     return (
-        <div className='flex items-center justify-center h-full md:h-[100vh] sm:p-[30px] md:p-[50px]'>
+        <div className='flex items-center justify-center sm:p-[30px] md:p-[50px]'>
             
             <div className="flex flex-col w-full max-w-[1200px] p-[30px] gap-[20px] bg-slate-100 shadow-default">
                 <div>
@@ -16,7 +20,7 @@ export const BookPage = () => {
                             'Loading Book...'
                             :
                             <div className="grid gap-[20px]">
-                                <Link className="underline w-min" to='/'>Back</Link>
+                                <button onClick={goBackHandler} className="underline w-min">Back</button>
                                 <div className="flex flex-col items-center justify-center gap-[30px] md:flex-row  md:justify-start">
                                     <div className="bg-slate-200 flex justify-center items-center max-w-[200px] min-h-[250px] w-full shadow-default">
                                         <img src={data?.volumeInfo?.imageLinks?.thumbnail} alt="bookImg" />
